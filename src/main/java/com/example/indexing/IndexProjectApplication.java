2fad5a0fcaf376ebcf0e32b87fd5ec8f406c3457
sujit <sujittech06@gmail.com>
2024-05-18 01:11:15 +0530

    modified

--- src/main/java/com/example/indexing/IndexProjectApplication.java
+++ src/main/java/com/example/indexing/IndexProjectApplication.java
@@ -2,12 +2,20 @@ package com.example.indexing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class IndexProjectApplication {
+public class IndexProjectApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(IndexProjectApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(IndexingExController.class);
 	}
 
 }
