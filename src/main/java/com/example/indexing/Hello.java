e454056856dc3b6d150add45f0dd68db51375bd8
sujit-01 <sujittech06@gmail.com>
2024-09-02 11:50:20 +0530

    "commit"
    add file
    
    modified
    
    modified
    
    added hello
    
    "commit second"
    
    removed sujit
    
    commit kya hal hai
    
    commit sujit1
    
    commit sujit1
    
    commit
    
    commit1234
    
    "commit second"
    
    removed sujit
    
    commit kya hal hai
    
    commit sujit1
    
    commit sujit1
    
    commit
    
    commit1234
    
    commit new output
    
    commit1234
    
    commit new output
    
    "commit"
    
    commit new output
    
    "commit"
    
    commit new output
    
    commit1234
    
    commit new output
    
    "commit"
    
    commit new output
    
    "commit"
    
    commit1234
    
    commit new output
    
    "commit"
    
    commit new output
    
    "commit"
    
    commit new output
    
    commit1234
    
    commit new output
    
    "commit"
    
    commit new output
    
    "commit"
    
    commit new output
    
    commit1234
    
    commit new output
    
    "commit"
    
    commit new output
    
    "commit"
    
    commit new output
    
    commit1234
    
    commit new output
    
    "commit"
    
    commit new output
    
    "commit"
    
    commit1234
    
    commit new output
    
    "commit"
    
    commit new output
    
    "commit"
    
    commit new output
    
    commit1234
    
    commit new output
    
    "commit"
    
    commit new output
    
    "commit"
    
    modified
    
    added hello
    
    "commit second"
    
    removed sujit
    
    commit kya hal hai
    
    commit sujit1
    
    commit sujit1
    
    commit
    
    commit1234
    
    "commit second"
    
    removed sujit
    
    commit kya hal hai
    
    commit sujit1
    
    commit sujit1
    
    commit
    
    commit1234
    
    commit new output
    
    commit1234
    
    commit new output
    
    "commit"
    
    commit new output
    
    "commit"
    
    commit new output
    
    commit1234
    
    commit new output
    
    "commit"
    
    commit new output
    
    "commit"
    
    commit1234
    
    commit new output
    
    "commit"
    
    commit new output
    
    "commit"
    
    commit new output
    
    commit1234
    
    commit new output
    
    "commit"
    
    commit new output
    
    "commit"
    
    commit new output
    
    commit1234
    
    commit new output
    
    "commit"
    
    commit new output
    
    "commit"
    
    commit new output
    
    commit1234
    
    commit new output
    
    "commit"
    
    commit new output
    
    "commit"
    
    commit1234
    
    commit new output
    
    "commit"
    
    commit new output
    
    "commit"
    
    commit new output
    
    commit1234
    
    commit new output
    
    "commit"
    
    commit new output
    
    "commit"
    
    modified
    
    added hello
    
    "commit second"
    
    removed sujit
    
    commit kya hal hai
    
    commit sujit1
    
    commit sujit1
    
    commit
    
    commit1234
    
    "commit second"
    
    removed sujit
    
    commit kya hal hai
    
    commit sujit1
    
    commit sujit1
    
    commit
    
    commit1234
    
    commit new output
    
    commit1234
    
    commit new output
    
    "commit"
    
    commit new output
    
    "commit"
    
    commit new output
    
    commit1234
    
    commit new output
    
    "commit"
    
    commit new output
    
    "commit"
    
    commit1234
    
    commit new output
    
    "commit"
    
    commit new output
    
    "commit"
    
    commit new output
    
    commit1234
    
    commit new output
    
    "commit"
    
    commit new output
    
    "commit"
    
    commit new output
    
    commit1234
    
    commit new output
    
    "commit"
    
    commit new output
    
    "commit"
    
    commit new output
    
    commit1234
    
    commit new output
    
    "commit"
    
    commit new output
    
    "commit"
    
    commit1234
    
    commit new output
    
    "commit"
    
    commit new output
    
    "commit"
    
    commit new output
    
    commit1234
    
    commit new output
    
    "commit"
    
    commit new output
    
    "commit"
    
    "commit second"
    
    removed sujit
    
    commit kya hal hai
    
    commit sujit1
    
    commit sujit1
    
    commit
    
    commit1234
    
    commit new output
    
    commit1234
    
    commit new output
    
    "commit"
    
    commit new output
    
    "commit"
    
    commit new output
    
    commit1234
    
    commit new output
    
    "commit"
    
    commit new output
    
    "commit"
    
    commit1234
    
    commit new output
    
    "commit"
    
    commit new output
    
    "commit"
    
    commit new output
    
    commit1234
    
    commit new output
    
    "commit"
    
    commit new output
    
    "commit"
    
    commit new output
    
    commit1234
    
    commit new output
    
    "commit"
    
    commit new output
    
    "commit"
    
    commit new output
    
    commit1234
    
    commit new output
    
    "commit"
    
    commit new output
    
    "commit"
    
    commit1234
    
    commit new output
    
    "commit"
    
    commit new output
    
    "commit"
    
    commit new output
    
    commit1234

--- src/main/java/com/example/indexing/Hello.java
+++ src/main/java/com/example/indexing/Hello.java
@@ -2,8 +2,8 @@ package com.example.indexing;
 
 public class Hello {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("heelo added 1234");
+       
     }
     
 }
