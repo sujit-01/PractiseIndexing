9829ea3f8c75ac0f954c9aa6e43b0b90edeba0ef
sujit-01 <sujittech06@gmail.com>
2024-09-09 22:55:28 +0530

    "commit"

--- src/main/java/com/example/indexing/Hello.java
+++ src/main/java/com/example/indexing/Hello.java
@@ -3,6 +3,8 @@ package com.example.indexing;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("heelo added 1234");
+        System.out.println("hello");
+        System.out.println("how r you");
     }
     
 }
